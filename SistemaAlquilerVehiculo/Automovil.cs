101030bd33993a58073062d210d82def83cf4b01
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-09-17 21:26:42 -0500

    se clea clase Vehiculo y Automovil

---
+++ SistemaAlquilerVehiculo/Automovil.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAlquilerVehiculo
+{
+    internal class Automovil
+    {
+    }
+    public class Automovil : Vehiculo, IReserva, IDevuelve
+    {
+
+        public Automovil(string marca, string modelo, int anio, decimal precioAlquiler)
+             : base(marca, modelo, anio, precioAlquiler) { }
+
+
+        public void Devolver()
+        {
+
+            if (Estado == EstadoVehiculo.ALQUILADO)
+            {
+                Console.WriteLine($"El automóvil {Marca}, {Modelo} ha sido devuelto.");
+            }
+
+        }
+        public void Reservar()
+        {
+            if (Estado == EstadoVehiculo.DISPONIBLE)
+            {
+                CambiarEstado(EstadoVehiculo.ALQUILADO);
+                Console.WriteLine($"El automóvil {Marca}, {Modelo} ha sido reservado.");
+            }
+            else
+            {
+                Console.WriteLine("Este automóvil no está disponible.");
+            }
+        }
+
+    }
+}
