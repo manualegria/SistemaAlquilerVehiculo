222e834b26d002fbd744882502f2d06bdd0d0ea0
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-09-17 19:56:04 -0500

    creacion de estructura de carpeta , enums e interfaces

---
+++ SistemaAlquilerVehiculo/enums/EstadoVehiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAlquilerVehiculo.enums
+{
+    public enum EstadoVehiculo
+    {
+
+        DISPONIBLE,
+        ALQUILADO,
+        MANTENIMIENTO
+    }
+}
---
+++ SistemaAlquilerVehiculo/interfaces/IReserva.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAlquilerVehiculo.interfaces
+{
+    public interface IReserva
+    {
+
+        void Reservar(Cliente cliente, DateTime fechaInicio, DateTime fechaFin);
+        void Reservar();
+    }
+}
