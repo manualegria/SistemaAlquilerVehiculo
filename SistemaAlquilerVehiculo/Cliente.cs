71d3340b55b54b46a8823da2f122b42086c45966
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-09-17 22:05:49 -0500

    se crea clase cliente

---
+++ SistemaAlquilerVehiculo/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAlquilerVehiculo
+{
+    //  cliente del sistema de alquiler de vehículos.
+    public class Cliente
+    {
+
+        public string Nombre { get; set; }
+        public string DNI { get; set; }
+
+
+        // Constructor de la clase Cliente.
+        public Cliente(string nombre, string dni)
+        {
+            Nombre = nombre;
+            DNI = dni;
+        }
+    }
+}
